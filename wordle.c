977b07c6e4f17875604a1c9e2213bc76daace89a
Mark Byrne <markbyrne477@gmail.com>
2025-02-04 12:55:41 +0000

    created wordle c file

---
+++ wordle.c
@@ -0,0 +1,16 @@
+// wordle.txt
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "wordle.h"
+
+int main();
+
+
+int main(int argc, char **argv)
+{
+    printf("Welcome to Wordle!\n");
+
+    return (0);
+}
